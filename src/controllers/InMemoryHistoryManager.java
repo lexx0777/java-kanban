b7e8ac4c79155606523031953e1a6b05b9fb08a8
Aleksey Krasilnikov <lexxei@yandex.ru>
2025-01-27 11:53:06 +0300

    Доработка по Спринту 6

--- src/controllers/InMemoryHistoryManager.java
+++ src/controllers/InMemoryHistoryManager.java
@@ -4,31 +4,82 @@ import model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
-    final private ArrayList<Task> history = new ArrayList<>();
-    final private int historySize;
+    final private HashMap<Integer, Node> historyMap = new HashMap<>(); //id, Node
+    private Node head;
+    private Node tail;
 
-    public InMemoryHistoryManager(int historySize) {
-        this.historySize = historySize;
+    private void removeNode(Node node){
+        if (node != null) {
+            if (node == tail
+                    && node == head) {
+                tail = null;
+                head = null;
+                node.setData(null);
+                node.setPrev(null);
+                node.setNext(null);
+            } else if (node == tail) {
+                tail = tail.getPrev();
+            } else if (node == head) {
+                head = head.getNext();
+            } else {
+                node.getPrev().setNext(node.getNext());
+                node.getNext().setPrev(node.getPrev());
+                node.setData(null);
+                node.setPrev(null);
+                node.setNext(null);
+            }
+        }
+    }
+
+    private Node linkLast(Task task) {
+        Node node = new Node(null, task, null);
+        if (task != null){
+            if (tail == null) {
+                head = node;
+                tail = node;
+            } else {
+                node.setPrev(tail);
+                tail.setNext(node);
+                tail = node;
+            }
+        }
+        historyMap.remove(node.getData().getId());
+        return node;
     }
 
     @Override
-    public boolean add(Task task) {
-        if (task == null) {
-            return false;
+    public void add(Task task) {
+        if (task != null) {
+            if (historyMap.containsKey(task.getId())) {
+                remove(task.getId());
+                historyMap.put(task.getId(), linkLast(task));
+            } else {
+                historyMap.put(task.getId(), linkLast(task));
+            }
+
         }
-        history.addLast(task);
-        if ( history.size() > historySize) {
-            history.removeFirst();
+    }
+
+    @Override
+    public void remove(int taskId) {
+        if (historyMap.containsKey(taskId)) {
+            removeNode(historyMap.get(taskId));
         }
-        return true;
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        List<Task> tasksList = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            tasksList.add(node.getData());
+            node = node.getNext();
+        }
+        return tasksList;
     }
 }
 
--- test/controllersTest/InMemoryHistoryManagerTest.java
+++ test/controllersTest/InMemoryHistoryManagerTest.java
@@ -21,29 +21,47 @@ class InMemoryHistoryManagerTest {
 
     @Test
     public void shouldAddAndGetHistory() {
-        Task task1 = new Task(0,"Убраться в комнате", "Протереть пыль и пр", TaskStatus.NEW);
-        Epic epic1 = new Epic(0,"Сделать дипломную работу", "успеть за месяц");
-        Subtask subtask1 = new Subtask(0,"Сделать дело1", "10 микродел",
+        Task task1 = new Task(1,"Убраться в комнате", "Протереть пыль и пр", TaskStatus.NEW);
+        Epic epic1 = new Epic(2,"Сделать дипломную работу", "успеть за месяц");
+        Subtask subtask1 = new Subtask(3,"Сделать дело1", "10 микродел",
                 TaskStatus.NEW, epic1.getId());
         historyManager.add(task1);
+        assertEquals(1, historyManager.getHistory().size());
+        historyManager.add(task1);
+        assertEquals(1, historyManager.getHistory().size());
         historyManager.add(epic1);
+        assertEquals(2, historyManager.getHistory().size());
         historyManager.add(subtask1);
+        assertEquals(3, historyManager.getHistory().size());
+        historyManager.add(task1);
         assertEquals(3, historyManager.getHistory().size());
         assertTrue(historyManager.getHistory().contains(task1));
         assertTrue(historyManager.getHistory().contains(epic1));
         assertTrue(historyManager.getHistory().contains(subtask1));
+
+        historyManager.remove(subtask1.getId());
+        assertEquals(2, historyManager.getHistory().size());
     }
 
     @Test
     void shouldRemoveFirstTaskWhenAddNewTaskAndListAreFull() {
-        Task task11 = new Task(0,"Задача 11", "Описание", TaskStatus.NEW);
-        for (int i = 0; i < 10; i++) {
-            historyManager.add(new Task(0,"Задача " + i, "Описание", TaskStatus.NEW));
+        Task task11 = new Task(101,"Задача 101", "Описание", TaskStatus.NEW);
+        for (int i = 0; i < 100; i++) {
+            historyManager.add(new Task(i,"Задача " + i, "Описание", TaskStatus.NEW));
         }
         historyManager.add(task11);
 
-        assertEquals(10, historyManager.getHistory().size());
-        assertEquals(task11, historyManager.getHistory().get(9));
+        assertEquals(101, historyManager.getHistory().size());
+        assertEquals(task11, historyManager.getHistory().get(100));
 
+        historyManager.add(task11);
+        assertEquals(task11, historyManager.getHistory().getLast());
+
+        Task task5 = new Task(5,"Задача 5", "Описание", TaskStatus.NEW);
+        for (int i = 0; i < 25; i++) {
+            historyManager.add(task5);
+        }
+        assertEquals(101, historyManager.getHistory().size());
+        assertEquals(task5, historyManager.getHistory().getLast());
     }
 }
